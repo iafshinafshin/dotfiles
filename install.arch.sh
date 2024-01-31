b3dd46a176c2a75dc2985e4766610f4b1fddf5b4
Afshin <iafshinafshin@gmail.com>
2024-01-31 18:22:39 +0330

    Zsh :i split zsh config from install.arch.sh

--- install.arch.sh
+++ install.arch.sh
@@ -1,7 +1,6 @@
 #!/bin/env bash
 cd
 sudo pacman -Sy
-sudo pacman -Syu
 sudo pacman -S neovim git thunar vim vi cmus base-devel zsh
 echo "INSTALL PARU & YAY"
 cd /opt
@@ -10,10 +9,10 @@ sudo git clone https://aur.archlinux.org/paru.git
 
 sudo chown -R $USER:$USER yay paru
 
-cd yay 
+cd yay
 makepkg -si
 cd ..
-cd paru 
+cd paru
 makepkg -si
 mkaepkg -si
 
@@ -21,12 +20,12 @@ cd ~/
 
 paru -S wofi mako thunar ttf-jetbrains-mono-nerd noto-fonts-emoji starship grim gvfs lxappearance
 
-paru -s python2 node nodejs npm visual-studio-code-bin mpv zsh fish feh neovim alacritty exa 
+paru -s python2 node nodejs npm visual-studio-code-bin mpv zsh fish feh neovim alacritty exa
 
 echo "create my directorys"
 mkdir ct
 mkdir ft
-mkdir Downloads 
+mkdir Downloads
 mkdir Pictures
 
 echo "INSTALL PACKER.NVIM"
@@ -34,12 +33,12 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvi
 
 echo "create my shortcuts in zsh "
 
-echo 'alias ll="exa --icons -lrh -t=created"' >> .zshrc
+echo 'alias ll="exa --icons -lrh -t=created"' >>.zshrc
 echo 'alias la="exa --icons -lrh -a -t=created"' >>.zshrc
-echo 'alias l="exa --icons"' >> .zshrc
-echo 'alias nv="nvim"' >> .zshrc
-echo 'alias cp="cp -rvf"' >> .zshrc
-echo 'alias rm="rm -rvf"' >> .zshrc
+echo 'alias l="exa --icons"' >>.zshrc
+echo 'alias nv="nvim"' >>.zshrc
+echo 'alias cp="cp -rvf"' >>.zshrc
+echo 'alias rm="rm -rvf"' >>.zshrc
 
 echo "install snap store"
 
@@ -52,7 +51,6 @@ sudo ln -s /var/lib/snapd/snap /snap
 
 echo "installing oh my zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
 
 echo "install zsh plugins"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
