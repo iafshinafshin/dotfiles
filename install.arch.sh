37422bc98b64ba31736d378d19d9a9cb00fa17d1
Afshin <iafshinafshin@gmail.com>
2024-01-31 18:30:02 +0330

    Zsh configs : i remove zsh config and write them removed code in zsh-setup.sh

--- install.arch.sh
+++ install.arch.sh
@@ -33,28 +33,9 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvi
 
 echo "create my shortcuts in zsh "
 
-echo 'alias ll="exa --icons -lrh -t=created"' >>.zshrc
-echo 'alias la="exa --icons -lrh -a -t=created"' >>.zshrc
-echo 'alias l="exa --icons"' >>.zshrc
-echo 'alias nv="nvim"' >>.zshrc
-echo 'alias cp="cp -rvf"' >>.zshrc
-echo 'alias rm="rm -rvf"' >>.zshrc
-
-echo "install snap store"
-
 git clone https://aur.archlinux.org/snapd.git
 cd snapd
 makepkg -si
 sudo systemctl enable --now snapd.socket
 sudo systemctl enable snapd
 sudo ln -s /var/lib/snapd/snap /snap
-
-echo "installing oh my zsh"
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "install zsh plugins"
-git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-echo "write this code in plugin zsh : "
-echo "zsh-syntax-highlighting"
-echo "zsh-autosuggestions"
---
+++ zsh-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/env bash
+
+echo "Upadte with pacman"
+
+sudo pacman -Sy
+
+echo "create my shortcuts in zsh "
+echo 'alias ll="exa --icons -lrh -t=created"' >>.zshrc
+echo 'alias la="exa --icons -lrh -a -t=created"' >>.zshrc
+echo 'alias l="exa --icons"' >>.zshrc
+echo 'alias nv="nvim"' >>.zshrc
+echo 'alias cp="cp -rvf"' >>.zshrc
+echo 'alias rm="rm -rvf"' >>.zshrc
+
+echo "installing oh my zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo "install zsh plugins"
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.zsh/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+echo "write this code in plugin zsh : "
+echo "zsh-syntax-highlighting"
+echo "zsh-autosuggestions"
