0a8fe9901877274ae9aeb587348f8dfd1c1f65ae
Afshin <iafshinafshin@gmail.com>
2024-02-06 23:23:55 +0330

    Shortcut key: add shortcut key for keyboard for linux capslock as esc and left alt + h/j/k/l

---
+++ key-bind.sh
@@ -0,0 +1,16 @@
+echo -e "[ids]" >>/etc/keyd/default.conf
+echo -e "" >>/etc/keyd/default.conf
+echo -e "*" >>/etc/keyd/default.conf
+echo -e "" >>/etc/keyd/default.conf
+echo -e "[main]" >>/etc/keyd/default.conf
+echo -e "" >>/etc/keyd/default.conf
+echo -e "capslock = overload(control, esc)" >>/etc/keyd/default.conf
+echo -e "" >>/etc/keyd/default.conf
+echo -e "alt = layer(nav)" >>/etc/keyd/default.conf
+echo -e "" >>/etc/keyd/default.conf
+echo -e "[nav]" >>/etc/keyd/default.conf
+echo -e "" /default.conf >>/etc/keyd
+echo -e "h = left" >>/etc/keyd/default.conf
+echo -e "j = down" >>/etc/keyd/default.conf
+echo -e "k = up" >>/etc/keyd/default.conf
+echo -e "l = right" >>/etc/keyd/default.conf
