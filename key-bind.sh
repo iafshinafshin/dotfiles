7fceb73f86c1b0870fd10bab3034e837ab4fdbeb
Afshin <iafshinafshin@gmail.com>
2024-02-10 15:44:41 +0330

    Keyd : added a new key bind (commented)

--- key-bind.sh
+++ key-bind.sh
@@ -2,19 +2,39 @@
 
 
 sudo su
-echo -e "[ids]" >>/etc/keyd/default.conf
-echo -e "" >>/etc/keyd/default.conf
-echo -e "*" >>/etc/keyd/default.conf
-echo -e "" >>/etc/keyd/default.conf
-echo -e "[main]" >>/etc/keyd/default.conf
-echo -e "" >>/etc/keyd/default.conf
-echo -e "capslock = overload(control, esc)" >>/etc/keyd/default.conf
-echo -e "" >>/etc/keyd/default.conf
-echo -e "alt = layer(nav)" >>/etc/keyd/default.conf
-echo -e "" >>/etc/keyd/default.conf
-echo -e "[nav]" >>/etc/keyd/default.conf
-echo -e "" /default.conf >>/etc/keyd
-echo -e "h = left" >>/etc/keyd/default.conf
-echo -e "j = down" >>/etc/keyd/default.conf
-echo -e "k = up" >>/etc/keyd/default.conf
-echo -e "l = right" >>/etc/keyd/default.conf
+[ids]
+ 
+*
+
+[main]
+
+capslock = overload(control, esc)
+
+alt = layer(nav)
+
+[nav]
+
+h = left
+j = down
+k = up
+l = right
+# [ids]
+#
+# *
+#
+# [main]
+#
+# #capslock = overload(control, esc)
+# capslock = layer(nav)
+# # rightalt = layer(nav)
+# # alt = layer(nav)
+# #rightshift = layer(nav)
+#
+# [nav:C]    
+#          
+# o = esc
+# h = left 
+# j = down 
+# k = up   
+# l = right
+# s = Control
