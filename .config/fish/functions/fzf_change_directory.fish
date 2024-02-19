3c02e58d51efe46ea051bceef05c5f3bd27e6b97
Afshin <iafshinafshin@gmail.com>
2024-02-19 23:08:24 +0330

    Fish : update change directory with fzf

--- .config/fish/functions/fzf_change_directory.fish
+++ .config/fish/functions/fzf_change_directory.fish
@@ -1,13 +1,20 @@
-function fish_user_key_bindings
-    # fzf
-    bind \cf fzf_change_directory
-
-    # vim-like
-    # bind \cl forward-char
-
-    # prevent iterm2 from closing when typing Ctrl-D (EOF)
-    # bind \cd delete-char
+function _fzf_change_directory
+    fzf | perl -pe 's/([ ()])/\\\\$1/g' | read foo
+    if [ $foo ]
+        builtin cd $foo
+        commandline -r ''
+        commandline -f repaint
+    else
+        commandline ''
+    end
 end
 
-# fzf plugin
-fzf_configure_bindings --directory=\co
+function fzf_change_directory
+    begin
+        echo $HOME/.config
+        echo $HOME/.config/nvim
+        find $(ghq root) -maxdepth 4 -type d -name .git | sed 's/\/\.git//'
+        ls -ad */ | perl -pe "s#^#$PWD/#" | grep -v \.git
+        # ls -ad $HOME/Developments/*/* |grep -v \.git
+    end | sed -e 's/\/$//' | awk '!a[$0]++' | _fzf_change_directory $argv
+end
