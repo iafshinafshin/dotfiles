5a6709260040b1daa4d37434b44732206518465f
Afshin <iafshinafshin@gmail.com>
2024-08-08 03:37:56 +0330

    feat(jsCoding file): that code with powershell

---
+++ .config/powershell/user_profile.ps1
@@ -0,0 +1,12 @@
+Set-Alias l ls
+Set-Alias c clear
+Set-Alias g git
+Set-Alias grep findstr
+Set-Alias tig 'C:\Program files\Git\usr\bin\tig.exe'
+Set-Alias less 'C:\Program files\Git\usr\bin\less.exe'
+Set-Alias vlc 'C:\Program Files\VideoLAN\VLC\vlc.exe'
+Set-Alias mpv 'C:\Program Files\VideoLAN\MPV\mpv.exe'
+Set-Alias jsCoding 'C:\Users\afshi\development\dotfiles\jsCoding.cmd'
+# Set Root Dir
+# $env:HOMEDRIVER = E:
+# Set-Location E:\Afshin\
--- jsCoding.cmd
+++ jsCoding.cmd
@@ -1,3 +1,4 @@
+powershell
 cd E:\l\JavaScript
 cd /d E:\l\JavaScript
 python3 .\py
